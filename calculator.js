ac826302d3c0baaf0706822c9a4bc2f27e51661e
Caitlin-Potter <caitjp@hotmail.com>
2019-10-06 18:02:56 +1300

    add to js file using pseudocode

---
+++ calculator.js
@@ -0,0 +1,56 @@
+var numString = ''
+var numArray = []
+let display = document.getElementById('display') 
+let isPreviousResult = false
+
+function buttonClick() {
+    document.addEventListener(click, buttonClick)
+}
+
+function getButtonValue() {
+    for (i=0; something < val; i++)
+
+    if num
+    else allClear
+    else clear 
+    else calculate 
+    else storeNumber
+}
+
+function number(button) {
+    //if buttons include decimals it will return
+}
+
+function allClear() {
+    numString = ''
+    numArray = []
+    display.value = 0
+}
+
+function clear() {
+    numString = ''
+    numArray = []
+}
+
+function storeNumber(button) {
+    // Create a storeNumber function for button (e.g function storeNumber (button) {}). Create an if/else statement. 
+    // - if numString is an empty string '' and the length of numArray is equal to 0 then return. If the variable numString is just an empty string '', then the length of numArray will equal -1 and
+    // the button value will be pushed in to the Array. 
+    // - else the string and button will be pushed in to the array, and numString will equal an empty string. 
+}
+
+function calculate () {
+    // Create a calculate function. This function needs to include the number object and the maths.abs function so that the absolute value of a number can be returned. 
+    // (e.g. let currentNumber = Number(numArray[0])) and create a for loop for numArray. 
+    // The calculate function needs to include reference to the symbols used to calculate the numbers on the board. 
+    // If the currentNumber is less than 0 it will use the Math.abs function to use the '-' symbol.
+    display.value = currentNumber
+    numString (JSON.stringify) = JSON.stringify(currentNumber)
+    isPreviousResult = true
+    numArray = []
+}
+
+
+
+
+
